fcf89d953c106539254d5a035c7afe28aeaf6768
officialragesh <rv4102@srmist.edu.in>
2026-04-14 14:00:02 +0530

    Coded UC7: Display Hello with multiple args using String.join() method

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,19 +1,12 @@
 public class HelloApp {
     public static void main(String[] args) {
+        // UC7: Using String.join() for automatic delimiter management
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            StringBuilder nameBuilder = new StringBuilder();
-            
-            // Enhanced For-Loop: Always add name + comma + space
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-            
-            // Clean up: Remove the very last ", " (last 2 characters)
-            String finalNames = nameBuilder.substring(0, nameBuilder.length() - 2);
-            
-            System.out.println("Hello, " + finalNames + "!");
+            // No loops, no substrings, no trailing commas!
+            String allNames = String.join(", ", args);
+            System.out.println("Hello, " + allNames + "!");
         }
     }
 }
